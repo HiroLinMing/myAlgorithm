1bbfd91dbd30b312309548c165e27c1844fc91fd
Tai-Jun Han <tai-jun.han@hpe.com>
2016-05-17 17:29:31 +0800

    add function

---
+++ Source.cpp
@@ -0,0 +1,167 @@
+﻿#include<iostream>
+
+#pragma region my
+
+using namespace std;
+int b[100][100] = {};
+
+void print_lcss(char x[], int i, int j) {
+	if (i == 0 || j == 0)return;
+	if (b[j][i] == 0) {
+		cout << x[i-1] << "  ";
+		print_lcss(x, i - 1, j - 1);
+		
+	}else if(b[j][i] == 2){
+		print_lcss(x, i, j - 1);
+	}else{
+		print_lcss(x, i - 1, j);
+	}
+}
+
+void main() {
+
+	char X[100] = {};
+	char Y[100] = {};
+	int table[100][100] = {};
+
+	cin >> X >> Y;
+
+	cout << strlen(X) << "   " << strlen(Y)<<'\n';
+
+	int m = strlen(X);
+	int n = strlen(Y);
+
+	for (int i = 0; i < m; i++) {
+		table[0][i] = 0;
+	}
+	for (int j = 0; j < n; j++) {
+		table[j][0] = 0;
+	}
+
+	for (int j = 0; j < n; j++) {
+		for (int i = 0; i < m; i++) {
+			cout << table[j][i] << " - ";
+		}
+		cout << '\n';
+	}
+
+	for (int j = 0; j < n; j++) {
+		for (int i = 0; i < m; i++) {
+			if (X[i] == Y[j]) {
+				table[j + 1][i + 1] = table[j][i] + 1;
+				b[j + 1][i + 1] = 0;
+				cout << '\\';
+			}else{
+				if (table[j][i + 1] >= table[j + 1][i]) {
+					table[j + 1][i + 1] = table[j][i + 1];
+					b[j + 1][i + 1] = 2;
+					cout << '|';
+				}
+				else {
+					table[j + 1][i + 1] = table[j + 1][i];
+					b[j + 1][i + 1] = 1;
+					cout << ' ';
+				}
+			}
+			cout << table[j + 1][i + 1] << "  ";
+		}
+		cout << '\n';
+	}
+
+	for (int j = 0; j < n; j++) {
+		for (int i = 0; i < m; i++) {
+			cout << b[j + 1][i + 1]<<"  ";
+		}
+		cout << '\n';
+	}
+
+	//Print_LCS(X, strlen(Y), strlen(X));
+	print_lcss(X, strlen(X), strlen(Y));
+
+	system("pause");
+}
+
+#pragma endregion
+
+/*#include<cstring>
+using namespace std;
+
+int c[100][100]; // c[i][j]表示序列S1前i个元素和S2的前j个元素的LCS
+int b[100][100]; //便于求解最优解
+
+void LCS_Length(char x[], char y[]);
+void Print_LCS(char x[], int i, int j);
+
+int main()
+{
+	char X[100], Y[100];
+
+	while (cin >> X >> Y)
+	{
+		LCS_Length(X, Y);
+		Print_LCS(X, strlen(X), strlen(Y));
+		cout << endl;
+	}
+	return 0;
+}
+
+void LCS_Length(char x[], char y[])
+{
+	int m = strlen(x);
+	int n = strlen(y);
+	for (int i = 0; i <= m; i++)
+		c[i][0] = 0;
+	for (int i = 0; i <= n; i++)
+		c[0][i] = 0;
+
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			if (x[i] == y[j])
+			{
+				c[i + 1][j + 1] = c[i][j] + 1;
+				b[i + 1][j + 1] = 0;
+				cout << '\\';
+			}
+			else if (c[i][j + 1] >= c[i + 1][j])
+			{
+				c[i + 1][j + 1] = c[i][j + 1];
+				b[i + 1][j + 1] = 1;
+				cout << '|';
+			}
+			else
+			{
+				c[i + 1][j + 1] = c[i + 1][j];
+				b[i + 1][j + 1] = 2;
+				cout << ' ';
+			}
+			cout << c[i + 1][j + 1] << "  ";
+		}
+		cout << '\n';
+	}
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << b[i + 1][j + 1] << "  ";
+		}
+		cout << '\n';
+	}
+}
+
+void Print_LCS(char x[], int i, int j)
+{
+	if ((i == 0) || (j == 0))
+		return;
+	if (b[i][j] == 0)
+	{
+		Print_LCS(x, i - 1, j - 1);
+		cout << x[i - 1] << ' ';
+	}
+	else if (b[i][j] == 1)
+		Print_LCS(x, i - 1, j);
+	else
+		Print_LCS(x, i, j - 1);
+}
+*/
---
+++ string.cpp
@@ -0,0 +1,80 @@
+﻿#include<iostream>
+
+using namespace std;
+
+void LeftShiftOne(char* s, int n)
+{
+	char t = s[0];  //保存第一个字符
+	for (int i = 1; i < n; i++)
+	{
+		s[i - 1] = s[i];
+	}
+	s[n - 1] = t;
+}
+
+void LeftRotateString(char* s, int n, int m)
+{
+	while (m--)
+	{
+		LeftShiftOne(s, n);
+	}
+}
+
+void reverse(char *array, int from, int to) {
+	while(from < to){
+		char a = array[from];
+		array[from++] = array[to];
+		array[to--] = a;
+	}
+}
+
+void reverseOne(char *array) {
+	char t = array[0];
+	for (int i = 0; i < strlen(array)-1; i++) {
+		array[i] = array[i+1];
+	}
+	array[strlen(array) - 1] = t;
+}
+
+void reverseAll(char *array, int n) {
+	while (n--) {
+		reverseOne(array);
+	}
+}
+
+void main() {
+
+#pragma region
+	/*char str[100] = {};
+	char strRev[100] = {};
+	cin >> str;
+
+	for (int i = strlen(str); i > 0; i--) {
+		strRev[strlen(str) - i] = str[i-1];
+	}
+
+	int i = 0;
+	while (strRev[i] != '\0') {
+		cout << strRev[i];
+		i++;
+	}
+	system("pause");*/
+#pragma endregion array
+
+	char array[100] = {};
+	cin >> array;
+	//LeftRotateString(array, strlen(array), 2);
+	reverse(array, 0, 2);
+	cout << array << endl;
+
+	cin >> array;
+	reverse(array, 0, 1);
+	reverse(array, 2, strlen(array) - 1);
+	reverse(array, 0, strlen(array) - 1);
+	cout << array;
+
+	cin >> array;
+	reverseAll(array, 2);
+	cout << array;
+	system("pause");
+}
